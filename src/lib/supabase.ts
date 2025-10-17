59147352d13f5831fa9cae5834a4d304f2c13659
Myo Min <myo.min@fusionsol.com>
2025-10-17 16:45:52 +0700

    update authorized policies

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log('SB URL', import.meta.env.VITE_SUPABASE_URL); 
+console.log('SB ANON', import.meta.env.VITE_SUPABASE_ANON_KEY?.slice(0,10));
+
+
+console.log(import.meta.env.VITE_SUPABASE_URL);
+console.log(import.meta.env.VITE_SUPABASE_ANON_KEY?.slice(0,10));
+
+
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL!,
   import.meta.env.VITE_SUPABASE_ANON_KEY!
